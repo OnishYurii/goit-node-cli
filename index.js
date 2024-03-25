96d70ab5a632941b7bc555dd8fd1c427fb994905
OnishYurii <onishchenkoyurii@gmail.com>
2024-03-25 22:39:36 +0200

    final state

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const contacts = require("./src/contacts");
 
+const { program } = require("commander");
+
 const invokeAction = async ({ action, id, name, email, phone }) => {
   switch (action) {
     case "list":
@@ -8,17 +10,23 @@ const invokeAction = async ({ action, id, name, email, phone }) => {
     case "get":
       const contact = await contacts.getContactById(id);
       return console.log(contact);
+    case "remove":
+      const removedContact = await contacts.removeContact(id);
+      return console.log(removedContact);
     case "add":
       const newContact = await contacts.addContact({ name, email, phone });
       return console.log(newContact);
   }
 };
 
-// invokeAction({ action: "list" });
-// invokeAction({ action: "get", id: "Z5sbDlS7pCzNsnAHLtDJd" });
-// invokeAction({
-//   action: "add",
-//   name: "Yurii",
-//   email: "onik@gmail.com",
-//   phone: "+335224343",
-// });
+program
+  .option("-a, --action, <type>")
+  .option("-i, --id, <type>")
+  .option("-n, --name, <type>")
+  .option("-e, --email, <type>")
+  .option("-p, --phone, <type>");
+
+program.parse();
+
+const options = program.opts();
+invokeAction(options);
--- src/contacts.js
+++ src/contacts.js
@@ -15,6 +15,17 @@ const getContactById = async (contactId) => {
   return result || null;
 };
 
+const removeContact = async (contactId) => {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((item) => item.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  const [result] = contacts.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return result;
+};
+
 const addContact = async (data) => {
   const contacts = await listContacts();
   const newContact = {
@@ -30,4 +41,5 @@ module.exports = {
   listContacts,
   getContactById,
   addContact,
+  removeContact,
 };
